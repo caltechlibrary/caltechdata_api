0c18e25e91e6a07bca3a87bce6bbc64b56d7ad5f
Tom Morrell <tmorrell@caltech.edu>
2017-09-21 11:51:58 -0700

    Dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 setup(
         name = 'caltechdata_write',
+        version ='0.0.1',
         packages = find_packages(),
+        install_requires=[
+            'requests',
+            'datacite'
+        ]
     )
