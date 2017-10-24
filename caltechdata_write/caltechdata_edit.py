4c233fb60288c15efc44a7fc48c513ea3230d533
Tom Morrell <tmorrell@caltech.edu>
2017-10-24 11:22:03 -0700

    Harmonize add and edit functions

--- caltechdata_write/caltechdata_edit.py
+++ caltechdata_write/caltechdata_edit.py
@@ -67,3 +67,52 @@ def Caltechdata_edit(token,ids,metadata={},files={},delete={},production=False):
         c = session()
         response = c.post(url, headers=headers, data=dat)
         print(response.text)
+	return fjson['new'][0]['url']
+
+Caltechdata_add(token,ids,metadata={},files={},production=False):
+
+    #Adds file
+
+    #If files is a string - change to single value array
+    if isinstance(ids, int):
+        ids = [str(ids)]
+    if isinstance(ids, str):
+        ids = [ids]
+
+    if production == True:
+        url = "https://data.caltech.edu/submit/api/edit/"
+        api_url = "https://data.caltech.edu/api/record/"
+    else:
+        url = "https://cd-sandbox.tind.io/submit/api/edit/"
+        api_url = "https://cd-sandbox.tind.io/api/record/"
+
+    headers = {
+        'Authorization' : 'Bearer %s' % token,
+        'Content-type': 'application/json'
+    }
+
+    if metadata:
+        metadata = customize_schema.customize_schema(metadata)
+
+    fjson = {}
+
+    for idv in ids:
+        metadata['id'] = idv
+
+        if files:
+            # upload new
+            fileinfo = [send_s3(f, token, production) for f in files]
+
+            fjson['new'] = fileinfo
+            metadata['files'] = fjson
+
+        dat = json.dumps({'record': metadata})
+
+        outf = open('out.json','w')
+        outf.write(dat)
+
+        #print(dat)
+        c = session()
+        response = c.post(url, headers=headers, data=dat)
+        print(response.text)
+        return fjson['new'][0]['url']
--- caltechdata_write/caltechdata_write.py
+++ caltechdata_write/caltechdata_write.py
@@ -58,54 +58,6 @@ def send_s3(filepath,token,production=False):
 
     return(fileinfo)
 
-def Caltechdata_add(token,ids,metadata={},files={},production=False):
-
-    #Adds file
-
-    #If files is a string - change to single value array
-    if isinstance(ids, int):
-        ids = [str(ids)]
-    if isinstance(ids, str):
-        ids = [ids]
-    
-    if production == True:
-        url = "https://data.caltech.edu/submit/api/edit/"
-        api_url = "https://data.caltech.edu/api/record/"
-    else:
-        url = "https://cd-sandbox.tind.io/submit/api/edit/"
-        api_url = "https://cd-sandbox.tind.io/api/record/"
-
-    headers = {
-        'Authorization' : 'Bearer %s' % token,
-        'Content-type': 'application/json'
-    }
-
-    if metadata:
-        metadata = customize_schema.customize_schema(metadata)
-
-    fjson = {}
-
-    for idv in ids:
-        metadata['id'] = idv
-
-        if files:
-            # upload new
-            fileinfo = [send_s3(f, token, production) for f in files]
-
-            fjson['new'] = fileinfo
-            metadata['files'] = fjson
-
-        dat = json.dumps({'record': metadata})
-
-        outf = open('out.json','w')
-        outf.write(dat)
-
-        #print(dat)
-        c = session()
-        response = c.post(url, headers=headers, data=dat)
-        print(response.text)
-        return fjson['new'][0]['url']
-
 def Caltechdata_write(metadata,token,files=[],production=False):
 
     #If files is a string - change to single value array
