cd41a69ac6d90cbe3ba7c685872593e00bbaeee4
Tom Morrell <tmorrell@caltech.edu>
2017-11-22 11:41:48 -0800

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
         name = 'caltechdata_api',
-        version ='0.0.1',
+        version ='0.0.2',
         packages = find_packages(),
         install_requires=[
             'requests',
