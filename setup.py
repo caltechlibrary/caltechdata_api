1fd64510974281552e1030ac8f8ed461103a5814
Tom Morrell <tmorrell@caltech.edu>
2017-12-11 08:24:40 -0800

    Increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
         name = 'caltechdata_api',
-        version ='0.0.2',
+        version ='0.0.3',
         packages = find_packages(),
         install_requires=[
             'requests',
