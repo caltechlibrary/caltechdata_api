a278cb9a3adbeb851d699bc1f4f8859146a8f8e4
Tom Morrell <tmorrell@caltech.edu>
2019-04-09 15:55:18 -0700

    Start of geo date update script

---
+++ edit_all_geo.py
@@ -0,0 +1,40 @@
+import argparse, os, json, requests,csv
+from caltechdata_api import caltechdata_edit, decustomize_schema
+
+#Get access token from TIND sed as environment variable with source token.bash
+token = os.environ['TINDTOK']
+
+production = True
+
+if production == True:
+    url = 'https://data.caltech.edu/api/records'
+else:
+    url = 'https://cd-sandbox.tind.io/api/records'
+
+response = requests.get(url+'/?size=1000&q=subjects:gps,thesis')
+hits = response.json()
+
+#Set up dictionary of links between resolver and thesis IDs
+available = os.path.isfile('data/record_list.csv')
+if available == False:
+    print("You need to run update_thesis_file.py")
+    exit()
+else:
+    record_list = {}
+    reader=csv.reader(open("data/record_list.csv"))
+    for row in reader:
+        record_list[row[1]] = row[0]
+
+for h in hits['hits']['hits']:
+    rid = str(h['id'])
+    print(rid)
+    record = decustomize_schema(h['metadata'],True)
+    if 'relatedIdentifiers' in record:
+        for r in record['relatedIdentifiers']:
+            if r['relationType']=='IsSupplementTo' and 'relatedIdentifierType'=='URL':
+                idv = record_list[r['relatedIdentifier']]
+                print(idv)
+        #metadata =\
+                #{'descriptions':[{'description':description,'descriptionType':'Abstract'}]}
+            #response = caltechdata_edit(token, rid, metadata, {}, {}, production)
+            #print(response)
