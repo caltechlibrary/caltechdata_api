018ea895380320bb2412320b123fcc2ab7c61578
Thomas Morrell <tmorrell@users.noreply.github.com>
2022-12-01 13:36:36 -0800

    Typo

--- caltechdata_api/caltechdata_write.py
+++ caltechdata_api/caltechdata_write.py
@@ -5,7 +5,7 @@ import os, requests
 import s3fs
 from requests import session
 from json.decoder import JSONDecodeError
-from caltechdata_aip import humanbytes, customize_schema
+from caltechdata_api import humanbytes, customize_schema
 
 
 def write_files_rdm(files, file_link, headers, f_headers, s3=None):
