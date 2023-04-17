ff78e00a24fcae92d3383f09b51e6cd910697aa5
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-04-17 16:25:44 -0700

    Initial working version

--- process_tomograms.py
+++ process_tomograms.py
@@ -13,7 +13,7 @@ def parse_collaborators(collaborator_string):
         model="text-davinci-003",
         prompt=f"""Take the string '{collaborator_string}', find the names and
     contributions, split the names into first and last names, and return in
-    the format [ first_name ; last_name ; contribution , ... ]  :""",
+    the format [ first_name ; last_name ; contribution ]  :""",
         temperature=0,
         max_tokens=100,
         top_p=1,
@@ -25,6 +25,7 @@ def parse_collaborators(collaborator_string):
     raw = response["choices"][0]["text"].split("\n\n")[1]
     for line in raw.split(","):
         split = line.strip("[]").split(";")
+        print(split)
         contributors.append(split)
     formatted = []
     for c in contributors:
@@ -39,8 +40,63 @@ def parse_collaborators(collaborator_string):
     return formatted
 
 
+def create_detailed_description(information, files):
+    keywords = []
+    description = "Details of data collection: "
+    keyword_labels = ["Microscope", "Tilt Scheme", "acquisitionSoftware"]
+    if "tiltSeriesCollection" in information:
+        info = information["tiltSeriesCollection"]
+        for key in info:
+            if key in keyword_labels:
+                keywords.append({"subject": info[key]})
+                description += f"{key}: {info[key]}"
+    if "uploadMethod" in information:
+        description += f'uploadMethod: {information["uploadMethod"]}'
+        keywords.append(information["uploadMethod"])
+    if "processingSoftwareUsed" in information:
+        description += f'uploadMethod: {information["processingSoftwareUsed"]}'
+        keywords.append(information["processingSoftwareUsed"])
+    for f in files:
+        if "reconstruction" in f:
+            rec = f["reconstruction"]
+            if "pixelSize(nm)" in rec:
+                description += f'reconstructionPixelSize(nm): {rec["pixelSize(nm)"]}'
+        if "rawTiltSeries" in f:
+            raw = f["rawTiltSeries"]
+            if "pixelSize(nm)" in raw:
+                description += f'rawPixelSize(nm): {raw["pixelSize(nm)"]}'
+    return description, keywords
+
+
+def get_formats(files):
+    formats = []
+    for f in files:
+        formats.append(f["fileName"].split(".")[-1])
+    return formats
+
+
+funding = [
+    {"funderName": "NIH"},
+    {"funderName": "HHMI"},
+    {"funderName": "Beckman Institute"},
+    {
+        "funderIdentifier": "grid.452959.6",
+        "funderIdentifierType": "GRID",
+        "funderName": "Gordon and Betty Moore Foundation",
+    },
+    {
+        "funderIdentifier": "grid.410514.5",
+        "funderIdentifierType": "GRID",
+        "funderName": "Agouron Institute",
+    },
+    {
+        "funderIdentifier": "grid.452951.e",
+        "funderIdentifierType": "GRID",
+        "funderName": "John Templeton Foundation",
+    },
+]
+
 directory = "jensen"
-dataset = "gca2021-09-10-347"
 
 files = glob.glob(f"{directory}/*.json")
 
@@ -50,6 +106,7 @@ for f in files:
         source = json.load(infile)
         annotation = source["annotation"][0]
         information = source["information"][0]
+        files = source["Files"]
 
         metadata = {}
         metadata["identifiers"] = [
@@ -100,5 +157,25 @@ for f in files:
                 "description": f"Electron tomography files of {title} ",
             }
         )
+        description, keywords = create_detailed_description(information, files)
+        descriptions.append({"descriptionType": "Other", "description": description})
         metadata["descriptions"] = descriptions
-        print(metadata)
+        metadata["formats"] = get_formats(files)
+        metadata["fundingReferences"] = funding
+        metadata["language"] = "eng"
+        metadata["publicationYear"] = 2023
+        metadata["publisher"] = "CaltechDATA"
+        metadata["types"] = {
+            "resourceTypeGeneral": "Dataset",
+            "resourceType": "Dataset",
+        }
+        metadata["rightsList"] = [{"rights": "cc-by-nc-4.0"}]
+        if "species/Specimen" in information:
+            for s in information["species/Specimen"]:
+                keywords.append(s["name"])
+        subjects = []
+        for k in keywords:
+            subjects.append({"subject": k})
+        metadata["subjects"] = subjects
+        metadata["titles"] = [{"title": title}]
+        print(json.dumps(metadata))
