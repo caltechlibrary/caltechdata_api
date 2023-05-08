b30930decc0f1213e44bcde86634da7fb8e1f171
Thomas Morrell <tmorrell@users.noreply.github.com>
2023-05-08 09:21:35 -0700

    Update get_files for new version of CaltechDATA

--- caltechdata_api/get_files.py
+++ caltechdata_api/get_files.py
@@ -1,23 +1,16 @@
 import argparse
-import csv
-import json
-import os
-
 import requests
-from datacite import schema40, schema43
-
-from caltechdata_api import decustomize_schema
 
 
-def get_files(idv, production=True, output=None):
-    # Returns file block or writes csv file
+def get_files(idv, production=True):
+    # Returns file block
 
     if production == True:
-        api_url = "https://data.caltech.edu/api/record/"
+        api_url = "https://data.caltech.edu/api/records/"
     else:
-        api_url = "https://cd-sandbox.tind.io/api/record/"
+        api_url = "https://data.caltechlibrary.dev/api/records/"
 
-    r = requests.get(api_url + str(idv))
+    r = requests.get(api_url + str(idv) + "/files")
     r_data = r.json()
     if "message" in r_data:
         raise AssertionError(
@@ -28,43 +21,29 @@ def get_files(idv, production=True, output=None):
             + " "
             + r_data["message"]
         )
-    if not "metadata" in r_data:
-        raise AssertionError("expected as metadata property in response, got " + r_data)
-    metadata = r_data["metadata"]
-
-    files = metadata["electronic_location_and_access"]
-
-    if output:
-        outfile = open(output, "w")
-        writer = csv.writer(outfile)
-        writer.writerow(["file_name", "file_size", "url", "embargo_status"])
-        for filem in files:
-            url = filem["uniform_resource_identifier"]
-            name = filem["electronic_name"][0]
-            writer.writerow([name, filem["file_size"], url, filem["embargo_status"]])
-
-    return files
+    if not "entries" in r_data:
+        raise AssertionError("expected as entries property in response, got " + r_data)
+    return r_data["entries"]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="get_files queries the caltechDATA (Invenio 3) API\
-    and returns file information in a csv file"
+    and returns file information"
     )
     parser.add_argument(
         "ids",
         metavar="ID",
-        type=int,
+        type=str,
         nargs="+",
         help="The CaltechDATA ID for each record of interest",
     )
     parser.add_argument("-test", dest="production", action="store_false")
-    parser.add_argument("-csv", dest="csv_file")
 
     args = parser.parse_args()
 
     production = args.production
-    csv_file = args.csv_file
 
     for idv in args.ids:
-        metadata = get_files(idv, production, csv_file)
+        metadata = get_files(idv, production)
+        print(metadata)
