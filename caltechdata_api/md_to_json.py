f5ca5fbe90550a335709fd71bbfc18f53144df55
Tom Morrell <tmorrell@caltech.edu>
2024-04-15 14:41:07 -0700

    Move and update md_to_json

---
+++ caltechdata_api/md_to_json.py
@@ -0,0 +1,116 @@
+import re
+import json
+
+class ReadmeFormatException(Exception):
+    """Custom exception for errors in the README format."""
+
+def camel_case(s):
+    """Converts a string to camelCase."""
+    s = re.sub(r"(\s|_|-)+", " ", s).title().replace(" ", "")
+    return s[0].lower() + s[1:] if s else ""
+
+def expand_special_keys(key, value):
+    """Expand special keys into their structured format (affiliation, nameIdentifiers)."""
+    if key == "affiliation":
+        return [
+            {
+                "affiliationIdentifier": value,
+                "affiliationIdentifierScheme": "ROR"
+            }
+        ]
+    elif key == "nameIdentifiers":
+        return [
+            {
+                "nameIdentifier": value,
+                "nameIdentifierScheme": "ORCID",
+                "schemeUri": f"https://orcid.org/{value}"
+            }
+        ]
+    return value
+
+def parse_readme_to_json(readme_path):
+    try:
+        with open(readme_path, 'r') as file:
+            lines = file.read().split('\n')
+    except IOError as e:
+        raise ReadmeFormatException(f"Failed to open or read the file: {e}")
+
+    json_data = {}
+    current_section = None
+    current_object = {}
+
+    section_pattern = re.compile(r'^##\s+(.*)$')
+    key_value_pattern = re.compile(r'^-\s+(.*?):\s+(.*)$')
+    link_pattern = re.compile(r'\[.*?\]\((.*?)\)')
+
+    for line_number, line in enumerate(lines, 1):
+        if not line.strip():
+            if current_object and current_section:
+                # check if current_object needs special treatment before appending
+                if len(current_object) == 1:
+                    key, value = next(iter(current_object.items()))
+                    if key in ["language", "publicationYear", "publisher", "version", "types"]:
+                        json_data[current_section].append(value)
+                    else:
+                        json_data[current_section].append(current_object)
+                else:
+                    json_data[current_section].append(current_object)
+                current_object = {}
+            continue
+
+        section_match = section_pattern.match(line)
+        if section_match:
+            if current_section and current_object:
+                # same check as above before starting a new section
+                if len(current_object) == 1:
+                    key, value = next(iter(current_object.items()))
+                    if key in ["language", "publicationYear", "publisher", "version"]:
+                        json_data[current_section].append(value)
+                    else:
+                        json_data[current_section].append(current_object)
+                else:
+                    json_data[current_section].append(current_object)
+                current_object = {}
+            current_section = camel_case(section_match.group(1))
+            json_data[current_section] = []
+            continue
+
+        key_value_match = key_value_pattern.match(line)
+        if key_value_match and current_section:
+            key, value = key_value_match.groups()
+            key = camel_case(key)
+
+            if key in ["affiliation", "nameIdentifiers"]:
+                value = expand_special_keys(key, value)
+            else:
+                link_match = link_pattern.search(value)
+                if link_match:
+                    value = link_match.group(1)
+
+            current_object[key] = value
+
+        elif line.strip() and not section_match:
+            raise ReadmeFormatException(f"Incorrect format detected at line {line_number}: {line}")
+
+    if current_section and current_object:
+        # final check for the last object
+        if len(current_object) == 1:
+            key, value = next(iter(current_object.items()))
+            if key in ["language", "publicationYear", "publisher", "version"]:
+                json_data[current_section].append(value)
+            else:
+                json_data[current_section].append(current_object)
+        else:
+            json_data[current_section].append(current_object)
+
+    return json_data
+
+readme_path = '/Users/elizabethwon/downloads/exampleREADME.md'
+try:
+    json_data = parse_readme_to_json(readme_path)
+    output_json_path = 'output1.json'
+    with open(output_json_path, 'w') as json_file:
+        json.dump(json_data, json_file, indent=4)
+    print(f"Converted JSON saved to {output_json_path}")
+except ReadmeFormatException as e:
+    print(f"Error parsing README file: {e}")
---
+++ templates/README.md
@@ -0,0 +1,83 @@
+# Metadata Information
+(using https://github.com/caltechlibrary/caltechdata_api/blob/main/example.json, please delete lines 1-2 when using this template.)
+## Identifiers
+- Identifier: 1924MNRAS..84..308E
+- Identifier Type: bibcode
+
+## Contributors
+- Name Type: Personal
+- Affiliation: [https://ror.org/04wxnsj81](https://ror.org/04wxnsj81)
+
+- Name: Contributor Name
+- Family Name: Family Name
+- Given Name: Given Name
+- Contributor Type: ContactPerson
+- Name Identifiers: [https://orcid.org/0000-0002-1825-0097](https://orcid.org/0000-0002-1825-0097)
+
+## Creators
+- Name Type: Personal
+- Affiliation: [https://ror.org/04wxnsj81](https://ror.org/04wxnsj81)
+- Name: Name
+- Family Name: Family Name
+- Given Name: Given Name
+- Name Identifiers: [https://orcid.org/0000-0002-1825-0097](https://orcid.org/0000-0002-1825-0097)
+
+## Dates
+- Date: 2014-10-01
+- Date Type: Created
+- Date: 2012-05-22/2016-12-21
+- Date Type: Collected
+
+## Descriptions
+- Description: Description
+- Description Type: Abstract
+
+## Formats
+- Format: format
+
+## Funding References
+- Award Title: Measurement of Column-Averaged CO2
+- Funder Name: National Aeronautics and Space Administration
+- Funder Identifier Type: GRID
+- Funder Identifier: grid.238252.c
+- Award Number: NAG5-12247
+
+## Language:
+- Language: eng
+
+## Publication Year
+- Publication Year: 2017
+
+## Publisher
+- Publisher: Publisher
+
+## Related Identifiers
+- Related Identifier: [http://www.url.org/](http://www.url.org/)
+- Related Identifier Type: URL
+- Relation Type: IsPartOf
+- Related Identifier: 10.5072/FK2
+- Related Identifier Type: DOI
+- Relation Type: IsDocumentedBy
+
+## Types
+- Resource Type General: Dataset
+- Resource Type: Dataset
+
+## Rights List
+- Rights: Rights Name
+- Rights URI: Rights List
+
+## Subjects
+- Subject: subject1
+- Subject: subject2
+
+## Titles
+- Title: Title
+- Title: Alternative Title
+- Title Type: AlternativeTitle
+
+## Version
+- Version: 0
+
+## Schema Version
+- Schema Version: [http://datacite.org/schema/kernel-4](http://datacite.org/schema/kernel-4)
