0bc1066d25da88c9b51ff147fdacc66772a5023c
RohanBhattaraiNP <152933030+RohanBhattaraiNP@users.noreply.github.com>
2024-11-30 21:04:51 -0800

    Update cli.py

--- caltechdata_api/cli.py
+++ caltechdata_api/cli.py
@@ -470,6 +470,7 @@ def create_record(production):
         elif choice == "create":
             args = parse_arguments()
             family_name, given_name = get_names(args["orcid"])
+            '''
             metadata = {
                 "titles": [{"title": args["title"]}],
                 "descriptions": [
@@ -504,6 +505,7 @@ def create_record(production):
                 "fundingReferences": args["fundingReferences"],
                 "schemaVersion": "http://datacite.org/schema/kernel-4",
             }
+            '''
             filepath, file_link = upload_supporting_file()
             if confirm_upload():
                 if filepath != "":
