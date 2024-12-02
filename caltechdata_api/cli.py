25d9135b4d78303503fa98fd037ed44c4d37c6c6
RohanBhattaraiNP <152933030+RohanBhattaraiNP@users.noreply.github.com>
2024-12-02 03:22:43 -0800

    Update cli.py

--- caltechdata_api/cli.py
+++ caltechdata_api/cli.py
@@ -475,7 +475,7 @@ def create_record(production):
                 "descriptions": [
                     {"description": args["description"], "descriptionType": "Abstract"}
                 ],
-                "publishers": "CaltechDATA",
+                "publisher": "CaltechDATA",
                 "creators": [
                     {
                         "affiliation": [
