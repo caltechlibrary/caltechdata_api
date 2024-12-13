3a1559df0121397cba96613af50244389015b413
Tom Morrell <tmorrell@users.noreply.github.com>
2024-12-13 08:07:11 -0800

    Add parse readme to package

--- caltechdata_api/__init__.py
+++ caltechdata_api/__init__.py
@@ -13,3 +13,4 @@ from .customize_schema import customize_schema
 from .get_metadata import get_metadata
 from .download_file import download_file, download_url
 from .utils import humanbytes
+from .md_to_json import parse_readme_to_json
