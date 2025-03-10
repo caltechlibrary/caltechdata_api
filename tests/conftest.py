78264ef0521dd19e8ebdadd80e856a6192bd3088
RohanBhattaraiNP <152933030+RohanBhattaraiNP@users.noreply.github.com>
2025-03-10 09:29:54 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,106 +27,6 @@ def full_datacite43_record():
                 ],
             }
         ],
-        "titles": [
-            {"title": "InvenioRDM"},
-            {
-                "title": "a research data management platform",
-                "titleType": "Subtitle",
-                "lang": "eng",
-            },
-        ],
-        "publisher": "InvenioRDM",
-        "publicationYear": "2018",
-        "subjects": [
-            {"subject": "custom"},
-            {
-                "subject": "Abdominal Injuries",
-                "subjectScheme": "MeSH",
-                "valueURI": "http://id.nlm.nih.gov/mesh/A-D000007",
-            },
-        ],
-        "contributors": [
-            {
-                "name": "Nielsen, Lars Holm",
-                "nameType": "Personal",
-                "contributorType": "Other",
-                "givenName": "Lars Holm",
-                "familyName": "Nielsen",
-                "nameIdentifiers": [
-                    {
-                        "nameIdentifier": "0000-0001-8135-3489",
-                        "nameIdentifierScheme": "ORCID",
-                    }
-                ],
-                "affiliation": [
-                    {
-                        "name": "CERN",
-                        "affiliationIdentifier": "01ggx4157",
-                        "affiliationIdentifierScheme": "ROR",
-                    }
-                ],
-            }
-        ],
-        "dates": [
-            {"date": "2018/2020-09", "dateType": "Issued"},
-            {"date": "1939/1945", "dateType": "Other", "dateInformation": "A date"},
-        ],
-        "language": "dan",
-        "identifiers": [
-            {"identifier": "10.5281/inveniordm.1234", "identifierType": "DOI"},
-            {"identifier": "1924MNRAS..84..308E", "identifierType": "bibcode"},
-        ],
-        "relatedIdentifiers": [
-            {
-                "relatedIdentifier": "10.1234/foo.bar",
-                "relatedIdentifierType": "DOI",
-                "relationType": "IsCitedBy",
-                "resourceTypeGeneral": "Dataset",
-            }
-        ],
-        "sizes": ["11 pages"],
-        "formats": ["application/pdf"],
-        "version": "v1.0",
-        "rightsList": [
-            {
-                "rights": "A custom license",
-                "rightsUri": "https://customlicense.org/licenses/by/4.0/",
-            },
-            {"rights": "No rightsUri license"},
-            {
-                "rights": "Creative Commons Attribution 4.0 International",
-                "rightsIdentifierScheme": "spdx",
-                "rightsIdentifier": "cc-by-4.0",
-                "rightsUri": "https://creativecommons.org/licenses/by/4.0/" "legalcode",
-            },
-        ],
-        "descriptions": [
-            {
-                "description": "<h1>A description</h1> <p>with HTML tags</p>",
-                "descriptionType": "Abstract",
-            },
-            {"description": "Bla bla bla", "descriptionType": "Methods", "lang": "eng"},
-        ],
-        "geoLocations": [
-            {
-                "geoLocationPoint": {
-                    "pointLatitude": -32.94682,
-                    "pointLongitude": -60.63932,
-                },
-                "geoLocationPlace": "test location place",
-            }
-        ],
-        "fundingReferences": [
-            {
-                "funderName": "European Commission",
-                "funderIdentifier": "00k4n6c32",
-                "funderIdentifierType": "ROR",
-                "awardTitle": "OpenAIRE",
-                "awardNumber": "246686",
-                "awardURI": ".../246686",
-            }
-        ],
-        "schemaVersion": "http://datacite.org/schema/kernel-4",
     }
 
 
@@ -178,32 +78,6 @@ def full_rdm_record():
                     "role": {"id": "other"},
                     "affiliations": [{"id": "01ggx4157"}],
                 }
-            ],
-            "dates": [
-                {"date": "1939/1945", "type": {"id": "other"}, "description": "A date"}
-            ],
-            "languages": [{"id": "dan"}],
-            "identifiers": [{"identifier": "1924MNRAS..84..308E", "scheme": "bibcode"}],
-            "related_identifiers": [
-                {
-                    "identifier": "10.1234/foo.bar",
-                    "scheme": "doi",
-                    "relation_type": {"id": "iscitedby"},
-                    "resource_type": {"id": "dataset"},
-                }
-            ],
-            "sizes": ["11 pages"],
-            "formats": ["application/pdf"],
-            "version": "v1.0",
-            "rights": [
-                {
-                    "title": {"en": "A custom license"},
-                    "link": "https://customlicense.org/licenses/by/4.0/",
-                },
-                {
-                    "title": {"en": "No rightsUri license"},
-                },
-                {"id": "cc-by-4.0"},
             ],
         }
     }
