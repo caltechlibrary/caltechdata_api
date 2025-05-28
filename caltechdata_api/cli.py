425c483e1fc24f3a17dcca11b0b50cc5b29ffa2c
Kshemaahna <63880115+Kshemaahna@users.noreply.github.com>
2025-05-28 12:58:51 -0700

    Update cli.py
    Implementation a feature such that users can choose a file from inside the current directory based on a displayed index instead of a filename, collision of filenames with indices handled with the help of a special character

--- caltechdata_api/cli.py
+++ caltechdata_api/cli.py
@@ -360,6 +360,7 @@ def upload_supporting_file(record_id=None):
     filepaths = []
     file_link = ""
     file_links = []
+    idx = 0
     while True:
         choice = get_user_input(
             "Do you want to upload or link data files? (upload/link/n): "
@@ -402,10 +403,10 @@ def upload_supporting_file(record_id=None):
             files = [
                 f for f in os.listdir() if not f.endswith(".json") and os.path.isfile(f)
             ]
-            print("\n".join(files))
+            print((""+ str(++idx) + "/ \n").join(files))
             while True:
                 filename = get_user_input(
-                    "Enter the filename to upload as a supporting file (or '*' to get all files currently in the directory, otherwise 'n' to finish): "
+                    "Enter the filename to upload as a supporting file (or '*' to get all files currently in the directory, or the index number of the file as displayed followed by a /, otherwise 'n' to finish): "
                 )
                 if filename == "*":
                     for files_name in files:
@@ -414,6 +415,11 @@ def upload_supporting_file(record_id=None):
                     print("All files added successfully")
                 elif filename == "n":
                     break
+                elif filename[len(filename)-1] == '/':
+                    files_name = files[int(filename[0])-1]
+                    filepath = os.path.abspath(files_name)
+                    filepaths.append(filepath)
+                    print("File added successfully")
                 elif filename in files:
                     file_size = os.path.getsize(filename)
                     if file_size > 1024 * 1024 * 1024:
